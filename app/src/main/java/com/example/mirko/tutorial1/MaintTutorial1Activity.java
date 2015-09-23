c2419758ad5050f5f79b96fa27118ac5e03d34f8
mirko <fcracker79@gmail.com>
2015-09-23 22:19:20 +0200

    Up to 258

--- app/src/main/java/com/example/mirko/tutorial1/MaintTutorial1Activity.java
+++ app/src/main/java/com/example/mirko/tutorial1/MaintTutorial1Activity.java
@@ -1,25 +1,12 @@
 package com.example.mirko.tutorial1;
 
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Adapter;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Gallery;
-import android.widget.GridView;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.ProgressBar;
-import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
-import android.widget.Toast;
 
-import java.util.Random;
-
-public class MaintTutorial1Activity extends AppCompatActivity {
+public class MaintTutorial1Activity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,29 +15,8 @@ public class MaintTutorial1Activity extends AppCompatActivity {
 
         final ArrayAdapter listAdapter =
                 new ArrayAdapter<>(this, android.R.layout.simple_list_item_multiple_choice,
-                        new String[]{"a", "b", "c", "d"});
-
-        final ListView listView = (ListView) findViewById(R.id.myAdapterListView);
-        listView.setAdapter(listAdapter);
-        listView.setSelection(R.id.selection);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(MaintTutorial1Activity.this,
-                        String.format("Item clicked: %s", parent.getItemAtPosition(position)),
-                        Toast.LENGTH_SHORT).show();
-
-            }
-        });
-        final Spinner spinner = findViewBy(R.id.myAdapterSpinner, Spinner.class);
-        spinner.setAdapter(listAdapter);
-
-        final GridView grid = findViewBy(R.id.myAdapterGridView, GridView.class);
-        grid.setAdapter(listAdapter);
-    }
-
-    private <T extends View> T findViewBy(int d, Class<T> clazz) {
-        return (T) findViewById(d);
+                        new String[]{"afa", "beta", "gamma", "delta"});
+        setListAdapter(listAdapter);
     }
 
     @Override
