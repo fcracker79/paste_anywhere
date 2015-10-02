486c916578fcdf1f9ab25d968ac756d8a86eb8d1
mirko <fcracker79@gmail.com>
2015-10-02 20:47:10 +0200

    Up to page 343

--- app/src/main/java/com/example/mirko/tutorial1/MaintTutorial1Activity.java
+++ app/src/main/java/com/example/mirko/tutorial1/MaintTutorial1Activity.java
@@ -2,15 +2,10 @@ package com.example.mirko.tutorial1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.text.format.DateUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,7 +21,7 @@ public class MaintTutorial1Activity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_maint_tutorial1, menu);
+        getMenuInflater().inflate(R.menu.action_bar_menu, menu);
 
         return true;
     }
@@ -38,12 +33,18 @@ public class MaintTutorial1Activity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.addMenuIem:
+                Toast.makeText(MaintTutorial1Activity.this, "Adding!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.deleteMenuItem:
+                Toast.makeText(MaintTutorial1Activity.this, "Removing!", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                throw new IllegalArgumentException("Unexpected " + id);
         }
 
-        return super.onOptionsItemSelected(item);
+        return true;
     }
 
     public void onClickGetDateTime(View v) {
