4dcc3d9bc2688c5fb6883ae49205f5d8df4d5aac
mirko <fcracker79@gmail.com>
2015-10-11 23:11:46 +0200

    First ViewPager

--- app/src/main/java/com/example/mirko/tutorial1/MaintTutorial1Activity.java
+++ app/src/main/java/com/example/mirko/tutorial1/MaintTutorial1Activity.java
@@ -1,11 +1,14 @@
 package com.example.mirko.tutorial1;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MaintTutorial1Activity extends AppCompatActivity {
+public class MaintTutorial1Activity extends AppCompatActivity implements ViewPagerFragment1.OnFragmentInteractionListener, ViewPagerFragment2.OnFragmentInteractionListener, ViewPagerFragment3.OnFragmentInteractionListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,12 +19,21 @@ public class MaintTutorial1Activity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_maint_tutorial1, menu);
 
+        final ViewPager vp = (ViewPager) findViewById(R.id.myPager);
+
+        vp.setAdapter(new MyPageAdapter(getSupportFragmentManager()));
         return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        Toast.makeText(MaintTutorial1Activity.this, "Cucu + " + uri, Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         return true;
     }
+
 
 }
