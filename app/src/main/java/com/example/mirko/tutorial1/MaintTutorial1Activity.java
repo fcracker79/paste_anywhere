e4bfbae7c899d55827b91a21debdfe29b0b27fa9
mirko <fcracker79@gmail.com>
2015-10-11 22:15:47 +0200

    Cleanup for ViewPager

--- app/src/main/java/com/example/mirko/tutorial1/MaintTutorial1Activity.java
+++ app/src/main/java/com/example/mirko/tutorial1/MaintTutorial1Activity.java
@@ -1,25 +1,15 @@
 package com.example.mirko.tutorial1;
 
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.LinearLayout;
-import android.widget.Toast;
 
-public class MaintTutorial1Activity extends AppCompatActivity implements MyButton.OnFragmentInteractionListener, ListOfElementsFragment.OnFragmentInteractionListener {
+public class MaintTutorial1Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maint_tutorial1);
-
-        final LinearLayout container = (LinearLayout) findViewById(R.id.containerForListFragment);
-        if (container.getChildCount() == 0) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.containerForListFragment, new ListOfElementsFragment())
-                    .commit();
-        }
     }
 
     @Override
@@ -34,13 +24,4 @@ public class MaintTutorial1Activity extends AppCompatActivity implements MyButto
         return true;
     }
 
-    @Override
-    public void onFragmentInteraction(Uri uri) {
-        Toast.makeText(this, String.format("onFragmentInteration(uri) %s", uri.toString()), Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onFragmentInteraction(String id) {
-        Toast.makeText(this, String.format("onFragmentInteration(id) %s", id), Toast.LENGTH_SHORT).show();
-    }
 }
