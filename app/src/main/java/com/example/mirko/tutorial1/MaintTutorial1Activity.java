1617a59d9346263476327d0e847386fb1561cc48
mirko <fcracker79@gmail.com>
2015-10-23 19:49:43 +0200

    First files stuff

--- app/src/main/java/com/example/mirko/tutorial1/MaintTutorial1Activity.java
+++ app/src/main/java/com/example/mirko/tutorial1/MaintTutorial1Activity.java
@@ -1,50 +1,53 @@
 package com.example.mirko.tutorial1;
 
-import android.app.Fragment;
-import android.app.FragmentTransaction;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.CheckBox;
 import android.widget.Toast;
 
-import com.braintreepayments.api.dropin.BraintreePaymentActivity;
-import com.example.mirko.tutorial1.states.PaymentOwner;
-import com.example.mirko.tutorial1.states.PaymentState;
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.google.gson.stream.JsonReader;
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.RequestParams;
-import com.loopj.android.http.TextHttpResponseHandler;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
-import java.io.StringReader;
-import java.util.Map;
-
-import cz.msebera.android.httpclient.Header;
-
-public class MaintTutorial1Activity extends AppCompatActivity implements CreateUserFragment.OnFragmentInteractionListener {
-    private static final String TAG_PAYMENT_FRAGMENT = "paymentFragment";
-    private static final String TAG_CREATE_USER_FRAGMENT = "createUserFragment";
-    private static final String TAG_SHOW_USER = "showUserFragment";
-
-    private static final int INTENT_CHOOSE_PAYMENT = 11235;
+public class MaintTutorial1Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maint_tutorial1);
 
-        if (getFragmentManager().findFragmentByTag(TAG_PAYMENT_FRAGMENT) == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.fragmentContent, new BarFragment(), TAG_PAYMENT_FRAGMENT)
-                    .commit();
+        InputStream is = null;
+        try {
+            is = getResources().getAssets().open("mirko/test_resources.txt");
+            final BufferedReader r = new BufferedReader(new InputStreamReader(is));
+            final StringBuilder sb = new StringBuilder();
+
+            for (String s = r.readLine(); s != null; s = r.readLine()) {
+                sb.append(String.format("\n%s", s));
+            }
+
+            final String localStorage =
+                    String.format(
+                            "\nfilesDir: %s\ncacheDir: %s",
+                            getFilesDir(),
+                            getCacheDir());
+            sb.append(String.format("\n%s", localStorage));
+            Toast.makeText(this, sb.toString(), Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    Log.e("MyTag", "My message", e);
+                }
+            }
         }
+
     }
 
     @Override
@@ -59,120 +62,4 @@ public class MaintTutorial1Activity extends AppCompatActivity implements CreateU
         return true;
     }
 
-    @Override
-    public void onCustomerCreated(String customerId) {
-        Toast.makeText(this, String.format("Customer [%s] created", customerId), Toast.LENGTH_SHORT)
-            .show();
-
-        ((PaymentOwner) getFragmentManager().findFragmentByTag(TAG_PAYMENT_FRAGMENT)).setCustomerId(customerId);
-
-        findViewById(R.id.createPaymentMethodButton).setEnabled(true);
-        findViewById(R.id.showUserDetails).setEnabled(true);
-    }
-
-    public void onCreateCustomerButtonClicked(View createCustomerButton) {
-        FragmentTransaction ft = getFragmentManager().beginTransaction();
-        Fragment prev = getFragmentManager().findFragmentByTag(TAG_CREATE_USER_FRAGMENT);
-        if (prev != null) {
-            ft.remove(prev);
-        }
-        ft.addToBackStack(null);
-
-        CreateUserFragment newFragment = CreateUserFragment.instance();
-        newFragment.setListener(this);
-        newFragment.show(ft, TAG_CREATE_USER_FRAGMENT);
-    }
-
-    public void onCreatePaymentMethodButtonClicked(View createPaymentMethodButton) {
-        AsyncHttpClient client = new AsyncHttpClient();
-        RequestParams params = new RequestParams();
-        params.put("customer_id",
-                ((PaymentOwner) getFragmentManager().findFragmentByTag(TAG_PAYMENT_FRAGMENT)).getCustomerId());
-
-        client.post(String.format("%s/generate_token", PaymentState.HOST), params, new TextHttpResponseHandler() {
-            @Override
-            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                Log.e("TOKEN", "Token FAILURE", throwable);
-                // throw new RuntimeException(throwable);
-            }
-
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, String token) {
-                Intent intent = new Intent(MaintTutorial1Activity.this, BraintreePaymentActivity.class);
-                intent.putExtra(BraintreePaymentActivity.EXTRA_CLIENT_TOKEN,
-                        token);
-                startActivityForResult(intent, INTENT_CHOOSE_PAYMENT);
-            }
-        });
-    }
-
-    public void onShowUserDetails(View v) {
-        AsyncHttpClient client = new AsyncHttpClient();
-        final String customerId =
-                ((PaymentOwner) getFragmentManager().findFragmentByTag(TAG_PAYMENT_FRAGMENT)).getCustomerId();
-
-        client.get(String.format("%s/customers/%s", PaymentState.HOST, customerId), new TextHttpResponseHandler() {
-            @Override
-            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                Log.e("TOKEN", "Token FAILURE", throwable);
-                // throw new RuntimeException(throwable);
-            }
-
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, String jsonCustomer) {
-                final Gson gson = new Gson();
-                final JsonParser p = new JsonParser();
-                final JsonReader reader = new JsonReader(new StringReader(jsonCustomer));
-
-                JsonObject result = p.parse(reader).getAsJsonObject().getAsJsonObject("result");
-                final CustomerDetail customer = gson.fromJson(result, CustomerDetail.class);
-
-                FragmentTransaction ft = getFragmentManager().beginTransaction();
-                Fragment prev = getFragmentManager().findFragmentByTag(TAG_SHOW_USER);
-                if (prev != null) {
-                    ft.remove(prev);
-                }
-                ft.addToBackStack(null);
-
-                ShowUserDetailsFragment newFragment = ShowUserDetailsFragment.newInstance(customer);
-                newFragment.show(ft, TAG_SHOW_USER);
-            }
-        });
-    }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        final boolean saveOnServer = ((CheckBox) findViewById(R.id.checkboxSaveToServer))
-                .isChecked();
-        if (requestCode == INTENT_CHOOSE_PAYMENT && saveOnServer) {
-            final String nonce = data.getStringExtra(BraintreePaymentActivity.EXTRA_PAYMENT_METHOD_NONCE);
-
-            AsyncHttpClient client = new AsyncHttpClient();
-            RequestParams params = new RequestParams();
-            params.put("nonce", nonce);
-
-            final String customerId = ((PaymentOwner) getFragmentManager().findFragmentByTag(TAG_PAYMENT_FRAGMENT))
-                    .getCustomerId();
-
-            client.post(
-                    String.format("%s/customers/%s/payment_methods", PaymentState.HOST, customerId),
-                    params,
-                    new TextHttpResponseHandler() {
-                @Override
-                public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                    Log.e("TOKEN", "Token FAILURE", throwable);
-                    // throw new RuntimeException(throwable);
-                }
-
-                @Override
-                public void onSuccess(int statusCode, Header[] headers, String responseString) {
-                    Toast.makeText(MaintTutorial1Activity.this,
-                            String.format("Payment method creation successful ? %s",
-                                    (String) new Gson().fromJson(responseString, Map.class).get("result")),
-                            Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
-    }
 }
