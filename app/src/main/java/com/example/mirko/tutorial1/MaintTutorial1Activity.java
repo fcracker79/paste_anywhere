36b70b984cb4174cc1d5bd7d1ccd3dbe1ae8d41b
mirko <fcracker79@gmail.com>
2015-10-23 20:51:17 +0200

    Digging into system files.

--- app/src/main/java/com/example/mirko/tutorial1/MaintTutorial1Activity.java
+++ app/src/main/java/com/example/mirko/tutorial1/MaintTutorial1Activity.java
@@ -5,12 +5,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MaintTutorial1Activity extends AppCompatActivity {
 
@@ -29,12 +38,6 @@ public class MaintTutorial1Activity extends AppCompatActivity {
                 sb.append(String.format("\n%s", s));
             }
 
-            final String localStorage =
-                    String.format(
-                            "\nfilesDir: %s\ncacheDir: %s",
-                            getFilesDir(),
-                            getCacheDir());
-            sb.append(String.format("\n%s", localStorage));
             Toast.makeText(this, sb.toString(), Toast.LENGTH_LONG).show();
         } catch (IOException e) {
             throw new IllegalStateException(e);
@@ -48,6 +51,68 @@ public class MaintTutorial1Activity extends AppCompatActivity {
             }
         }
 
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("Files dir : %s\n", getFilesDir()));
+        ((TextView) findViewById(R.id.localStorageText)).setText(sb);
+
+        final List<String> data = new ArrayList<>();
+
+        for (String sub : getFilesDir().list()) {
+            data.add("Files");
+            data.add(sub);
+        }
+
+        sb = new StringBuilder();
+        sb.append(String.format("Cache dir : %s", getCacheDir()));
+        ((TextView) findViewById(R.id.cacheStorageText)).setText(sb);
+
+        for (String sub : getCacheDir().list()) {
+            data.add("Cache");
+            data.add(sub);
+        }
+
+        final GridView v = (GridView) findViewById(R.id.fileListView);
+        v.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, data));
+
+        v.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                final int elementPosition = position / 2;
+                final String filename = data.get(elementPosition * 2 + 1);
+                final File dir = "Files".equals(data.get(elementPosition * 2)) ?
+                        getFilesDir() : getCacheDir();
+
+                final String fullPathName = new File(dir.toString(), filename).toString();
+
+                final StringBuilder content = new StringBuilder();
+
+                InputStream is = null;
+
+                try {
+                    is = new FileInputStream(fullPathName);
+                    final BufferedReader r = new BufferedReader(
+                            new InputStreamReader(is)
+                    );
+                    for (String s = r.readLine(); s != null; s = r.readLine()) {
+                        content.append(s).append("\n");
+                    }
+                    Toast.makeText(MaintTutorial1Activity.this, content, Toast.LENGTH_LONG).show();
+                } catch(IOException e) {
+                    Toast.makeText(
+                            MaintTutorial1Activity.this,
+                            String.format("Error opening %s: %s", fullPathName, e.getMessage()),
+                            Toast.LENGTH_LONG).show();
+                } finally {
+                    if (is != null) {
+                        try {
+                            is.close();
+                        } catch (IOException e) {
+                            Log.e("Boh", e.getMessage(), e);
+                        }
+                    }
+                }
+            }
+        });
     }
 
     @Override
